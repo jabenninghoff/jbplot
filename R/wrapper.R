ec21602ea913cb446feeddc60d966027e36ff687
John Benninghoff <jbenninghoff@mac.com>
2023-11-27 20:49:32 -0600

    Update wrapper styling

--- R/wrapper.R
+++ R/wrapper.R
@@ -1,3 +1,6 @@
+# convenience functions aren't tested and are excluded from code coverage
+# nolint start: object_name_linter. ggplot2 functions include dotted.case parameters.
+
 #' Robust linear smoothing
 #'
 #' A wrapper for [ggplot2::geom_smooth()] that sets `method` to [MASS::rlm()] and `formula` to
@@ -12,20 +15,29 @@
 #'   geom_point() +
 #'   geom_rlm()
 #' @export
-geom_rlm <- function(mapping = NULL,
-                     data = NULL,
-                     position = "identity",
-                     ...,
-                     method = MASS::rlm,
-                     formula = y ~ x,
-                     se = TRUE,
-                     na.rm = FALSE, # nolint: object_name_linter.
-                     orientation = NA,
-                     show.legend = NA, # nolint: object_name_linter.
-                     inherit.aes = TRUE) { # nolint: object_name_linter.
+geom_rlm <- function(
+    mapping = NULL,
+    data = NULL,
+    position = "identity",
+    ...,
+    method = MASS::rlm,
+    formula = y ~ x,
+    se = TRUE,
+    na.rm = FALSE,
+    orientation = NA,
+    show.legend = NA,
+    inherit.aes = TRUE) {
   ggplot2::geom_smooth(
-    mapping = mapping, data = data, position = position, ..., method = method, formula = formula,
-    se = se, na.rm = na.rm, orientation = orientation, show.legend = show.legend,
+    mapping = mapping,
+    data = data,
+    position = position,
+    ...,
+    method = method,
+    formula = formula,
+    se = se,
+    na.rm = na.rm,
+    orientation = orientation,
+    show.legend = show.legend,
     inherit.aes = inherit.aes
   )
 }
@@ -42,20 +54,29 @@ geom_rlm <- function(mapping = NULL,
 #'   geom_point() +
 #'   geom_lm()
 #' @export
-geom_lm <- function(mapping = NULL,
-                    data = NULL,
-                    position = "identity",
-                    ...,
-                    method = "lm",
-                    formula = y ~ x,
-                    se = TRUE,
-                    na.rm = FALSE, # nolint: object_name_linter.
-                    orientation = NA,
-                    show.legend = NA, # nolint: object_name_linter.
-                    inherit.aes = TRUE) { # nolint: object_name_linter.
+geom_lm <- function(
+    mapping = NULL,
+    data = NULL,
+    position = "identity",
+    ...,
+    method = "lm",
+    formula = y ~ x,
+    se = TRUE,
+    na.rm = FALSE,
+    orientation = NA,
+    show.legend = NA,
+    inherit.aes = TRUE) {
   ggplot2::geom_smooth(
-    mapping = mapping, data = data, position = position, ..., method = method, formula = formula,
-    se = se, na.rm = na.rm, orientation = orientation, show.legend = show.legend,
+    mapping = mapping,
+    data = data,
+    position = position,
+    ...,
+    method = method,
+    formula = formula,
+    se = se,
+    na.rm = na.rm,
+    orientation = orientation,
+    show.legend = show.legend,
     inherit.aes = inherit.aes
   )
 }
@@ -74,22 +95,32 @@ geom_lm <- function(mapping = NULL,
 #' ggplot(mpg, aes(hwy)) +
 #'   geom_hist_bw(binwidth = 1)
 #' @export
-geom_hist_bw <- function(mapping = NULL,
-                         data = NULL,
-                         position = "stack",
-                         color = "black",
-                         fill = "white",
-                         ...,
-                         binwidth = NULL,
-                         bins = NULL,
-                         na.rm = FALSE, # nolint: object_name_linter.
-                         orientation = NA,
-                         show.legend = NA, # nolint: object_name_linter.
-                         inherit.aes = TRUE) { # nolint: object_name_linter.
+geom_hist_bw <- function(
+    mapping = NULL,
+    data = NULL,
+    position = "stack",
+    color = "black",
+    fill = "white",
+    ...,
+    binwidth = NULL,
+    bins = NULL,
+    na.rm = FALSE,
+    orientation = NA,
+    show.legend = NA,
+    inherit.aes = TRUE) {
   ggplot2::geom_histogram(
-    mapping = mapping, data = data, position = position, color = color, fill = fill, ...,
-    binwidth = binwidth, bins = bins, na.rm = na.rm, orientation = orientation,
-    show.legend = show.legend, inherit.aes = inherit.aes
+    mapping = mapping,
+    data = data,
+    position = position,
+    color = color,
+    fill = fill,
+    ...,
+    binwidth = binwidth,
+    bins = bins,
+    na.rm = na.rm,
+    orientation = orientation,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes
   )
 }
 
@@ -115,20 +146,33 @@ geom_hist_bw <- function(mapping = NULL,
 #'   annotate_quo("label", x = 5.5, y = 30, label = "hwy increases for displ greater than 5.5") +
 #'   theme_quo()
 #' @export
-annotate_quo <- function(geom,
-                         x = NULL,
-                         y = NULL,
-                         xmin = NULL,
-                         xmax = NULL,
-                         ymin = NULL,
-                         ymax = NULL,
-                         xend = NULL,
-                         yend = NULL,
-                         family = "Lato",
-                         ...,
-                         na.rm = FALSE) { # nolint: object_name_linter.
+annotate_quo <- function(
+    geom,
+    x = NULL,
+    y = NULL,
+    xmin = NULL,
+    xmax = NULL,
+    ymin = NULL,
+    ymax = NULL,
+    xend = NULL,
+    yend = NULL,
+    family = "Lato",
+    ...,
+    na.rm = FALSE) {
   ggplot2::annotate(
-    geom = geom, x = x, y = y, xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, xend = xend,
-    yend = yend, family = family, ..., na.rm = na.rm
+    geom = geom,
+    x = x,
+    y = y,
+    xmin = xmin,
+    xmax = xmax,
+    ymin = ymin,
+    ymax = ymax,
+    xend = xend,
+    yend = yend,
+    family = family,
+    ...,
+    na.rm = na.rm
   )
 }
+
+# nolint end
