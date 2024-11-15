f604eb69cb4801998df10c2ca166dd0c83b4f07d
John Benninghoff <jbenninghoff@mac.com>
2024-11-14 21:00:07 -0600

    Add direction parameter to pal_differently()

--- tests/testthat/test-differently.R
+++ tests/testthat/test-differently.R
@@ -6,6 +6,10 @@ test_that("pal_differently validates parameters", {
   expect_error(pal_differently(name = NA), "'name'")
   expect_error(pal_differently(name = ""), "'name'")
   expect_error(pal_differently(name = "notdifferently"), "'name'")
+  expect_error(pal_differently(direction = NA), "'direction'")
+  expect_error(pal_differently(direction = 0), "'direction'")
+  expect_error(pal_differently(direction = 1.5), "'direction'")
+  expect_error(pal_differently(direction = -1.5), "'direction'")
   expect_no_error(pal_differently(name = "primary"))
 })
 
@@ -27,4 +31,10 @@ test_that("pal_differently function returns an unnamed vector of correct length"
   expect_identical(pal_differently(name = "plot")(1), unname(differently_palettes$plot[1]))
   expect_identical(pal_differently(name = "plot")(3), unname(differently_palettes$plot[1:3]))
   expect_identical(pal_differently(name = "plot")(6), unname(differently_palettes$plot))
+})
+
+test_that("pal_differently direction reverses vector order", {
+  expect_identical(
+    pal_differently(name = "primary", direction = -1)(6), rev(unname(differently_palettes$primary))
+  )
 })
